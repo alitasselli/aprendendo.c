213952a63e58ec147e14258763b0c9b3bd025eb9
Alicia Tasselli <at@cesar.school>
2019-09-10 15:00:12 -0300

    Add files via upload

---
+++ linhasarq.c
@@ -0,0 +1,16 @@
+//questao 18
+// escreva um programa para determinar o numero de linhas de um arquivo
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+FILE *arquivo;
+arquivo= fopen("usuarios.txt", "r");
+char c;
+int linhas=0;
+while(fread (&c, sizeof(char), 1, arquivo)){
+    printf("%c\n", c);
+    if(c == '\n')
+        linhas++;
+}
+printf("%d\n", linhas);
+}
